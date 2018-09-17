639ff0cd259bc1255e1b53231abfebf5aab9d4b8
Brandon Molyneaux <Brandon@Brandons-MacBook-Pro.local>
2018-09-17 08:03:20 -0500

    added dictionaries for months, updated in main class file to get the dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,44 @@
+monthNumber = {
+    1 : ['January', 'Jan', 31],
+    2 : ['February', 'Feb', 27],
+    3 : ['March', 'Mar', 31],
+    4 : ['April', 'Apr', 30],
+    5 : ['May', 'May', 31],
+    6 : ['June', 'Jun', 30],
+    7 : ['July', 'Jul', 31],
+    8 : ['August', 'Aug', 31],
+    9 : ['September', 'Sep', 30],
+    10 : ['October', 'Oct', 31],
+    11 : ['November', 'Nov', 30],
+    12 : ['December', 'Dec', 31]
+}
+
+monthLongName = {
+    'April': [4, 'Apr', 30],
+    'August': [8, 'Aug', 31],
+    'December': [12, 'Dec', 31],
+    'February': [2, 'Feb', 27],
+    'January': [1, 'Jan', 31],
+    'July': [7, 'Jul', 31],
+    'June': [6, 'Jun', 30],
+    'March': [3, 'Mar', 31],
+    'May': [5, 'May', 31],
+    'November': [11, 'Nov', 30],
+    'October': [10, 'Oct', 31],
+    'September': [9, 'Sep', 30]
+}
+
+monthShortName = {
+    'Apr': [4, 'April', 30],
+    'Aug': [8, 'August', 31],
+    'Dec': [12, 'December', 31],
+    'Feb': [2, 'February', 27],
+    'Jan': [1, 'January', 31],
+    'Jul': [7, 'July', 31],
+    'Jun': [6, 'June', 30],
+    'Mar': [3, 'March', 31],
+    'May': [5, 'May', 31],
+    'Nov': [11, 'November', 30],
+    'Oct': [10, 'October', 31],
+    'Sep': [9, 'September', 30]
+}
